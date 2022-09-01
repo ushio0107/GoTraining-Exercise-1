4f6592f4338041c0f97ada9de278eab5c8ee5ecb
ushio0107 <ushio000107@gmail.com>
2022-09-01 22:48:27 +0800

    finish

--- main.go
+++ main.go
@@ -13,11 +13,10 @@ import (
 	"sync"
 
 	"github.com/gocolly/colly"
-	WebServe "github.com/training_ex1/Training_Program"
 )
 
 var (
-	port          string
+	outputPath    string
 	dir           string
 	outputDirName string
 
@@ -43,7 +42,7 @@ type ModelInfo struct {
 }
 
 func init() {
-	flag.StringVar(&port, "port", "8000", "local host server port")
+	flag.StringVar(&outputPath, "outputPath", ".", "the path of the output directory(the output file will store inside)")
 	flag.StringVar(&dir, "dir", ".", "the working directory ")
 	flag.StringVar(&outputDirName, "outputDirName", "/output", "the name of the directory which stored the output files")
 
@@ -51,7 +50,32 @@ func init() {
 
 }
 
-func webClamb(testCase *Info, output *os.File, file os.FileInfo, collUpper colly.Collector, collLower colly.Collector, port string, wg *sync.WaitGroup, ts *httptest.Server) {
+func webClamb(testCase *Info, file os.FileInfo, collUpper colly.Collector, collLower colly.Collector, wg *sync.WaitGroup) {
+	// * Host a local server
+	// .html file is not allowed to be crawled directly, hosting a local server
+	// to make the package colly to implement web crawled successly.
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		testData_file, err := os.Open(filepath.Join(dir, file.Name()))
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer testData_file.Close()
+		testData_content, err := ioutil.ReadAll(testData_file)
+		if err != nil {
+			fmt.Println(err)
+		}
+		w.Write(testData_content)
+	}))
+
+	// To avoid output files aren't exist, os.Create will create file if file doesn't exist,
+	// clear file if file already existed, output file name will as the same as input file
+	// also, to avoid double file format, .html will be taken out by strings.Replace
+	outputFilePath := filepath.Join(outputPath, outputDirName)
+	output, err := os.Create(filepath.Join(outputFilePath, strings.Replace(file.Name(), ".html", "", -1)+".csv"))
+	fmt.Println("Created output file: ", output.Name())
+	if err != nil {
+		fmt.Println("File is failed, err: ", err)
+	}
 	defer output.Close()
 
 	collUpper.OnHTML("table.matrix ", func(eTable *colly.HTMLElement) {
@@ -90,7 +114,7 @@ func webClamb(testCase *Info, output *os.File, file os.FileInfo, collUpper colly
 		// stored testCase.excelFormula, var type string
 		// * failed
 		// =COUNTIF("B9:BtestCaseNum", "x")
-		testCase.excelFormula = "\nfailed;"
+		testCase.excelFormula = "\nfailed;123"
 		for i := 0; i < testCase.testInfo.testNum; i++ {
 			if i < 25 {
 				testCase.excelFormula = testCase.excelFormula + "=COUNTIF(" + string(66+i) + "9:" + string(66+i) + strconv.Itoa(testCase.modelInfo.modelNum+8) + ", \"x\");"
@@ -132,14 +156,14 @@ func webClamb(testCase *Info, output *os.File, file os.FileInfo, collUpper colly
 			}
 		}
 
-		mux.Unlock()
+		//mux.Unlock()
 	})
 
 	// collUpper.Visit("http://localhost:" + port + "/" + file.Name())
 	collUpper.Visit((ts.URL))
 
 	collLower.OnHTML("table.matrix ", func(eTable *colly.HTMLElement) {
-		mux.Lock()
+		//mux.Lock()
 		testCaseNum := 0
 		saveResult := ""
 		testCase.testInfo.testResult = append(testCase.testInfo.testResult, "")
@@ -182,7 +206,7 @@ func webClamb(testCase *Info, output *os.File, file os.FileInfo, collUpper colly
 		mux.Unlock()
 	})
 
-	collUpper.Visit(ts.URL)
+	collLower.Visit(ts.URL)
 
 	outputFileWrite(testCase, output)
 	wg.Done()
@@ -230,17 +254,11 @@ func main() {
 		fmt.Println(err)
 	}
 
-	// * Host a local server
-	// .html file is not allowed to be crawled directly, hosting a local server with a port
-	// to make the package colly to implement web crawled successly.
-	go WebServe.WebServer(port, dir)
-
 	// * Output Dir Setting
 	// the output files and folder will be created under the test data directory,
 	// the files will include in the folder whose name is set by the user, or default "output"
 	// the folder will be created if it's not exist.
-	outputPath := filepath.Join(dir, outputDirName)
-	err = os.MkdirAll(outputPath, os.ModePerm)
+	err = os.MkdirAll(filepath.Join(outputPath, outputDirName), os.ModePerm)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -254,31 +272,15 @@ func main() {
 			},
 		}
 
-		if file.IsDir() || strings.Contains(file.Name(), ".html") == false {
-			fmt.Println(file.Name())
+		if file.IsDir() || !strings.Contains(file.Name(), ".html") {
+			// to prevent the input file is not .html file or it's a directory, set a condition if input file type is directory or
+			// the file type is not html, pass the file and go to the next one
 			continue
+
 		} else {
 			// if wg.Add(1) is outside else {}, when there are no file can be processed, the program will struck
 			wg.Add(1)
-
-			// To avoid output files aren't exist, os.Create will create file if file doesn't exist,
-			// clear file if file already existed, output file name will as the same as input file
-			// also, to avoid double file format, .html will be taken out by strings.Replace
-
-			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				testData_file, err := os.Open("./test_data/" + file.Name())
-				if err != nil {
-					fmt.Println(err)
-				}
-				b, err := ioutil.ReadAll(testData_file)
-				w.Write(b)
-			}))
-			output, err := os.Create(outputPath + "/" + strings.Replace(file.Name(), ".html", "", -1) + ".csv")
-			fmt.Println("Created output file: ", output.Name())
-			if err != nil {
-				fmt.Println("File is failed, err: ", err)
-			}
-			go webClamb(&testCase, output, file, *collUpper, *collLower, port, wg, ts)
+			go webClamb(&testCase, file, *collUpper, *collLower, wg)
 		}
 	}
 
